7962380378ae55387379ab3eb71dbb0455ef571f
Leonardo-ADS24 <leonardo.santiago@alunos.unis.edu.br>
2024-10-18 12:09:18 -0300

    exe 02,04

--- exercico_02/main.c
+++ exercico_02/main.c
@@ -1,7 +1,103 @@
 #include <stdio.h>
+#include <string.h>
+
+#define MAX 30
+
+typedef struct{
+    char compromisso[30];
+    int horario;
+} Compromisso;
+
+typedef struct{
+    Compromisso Compromisso[MAX];
+    int quantidade;
+}ListaCompromisso;
+
+void iniciarLista(ListaCompromisso *lista){
+    lista->quantidade = 0;
+}
+
+int incluirCompromisso(ListaCompromisso *lista, char compromisso[],int horario){
+    if(lista->quantidade < MAX){
+        int i;
+        for( i = lista->quantidade -1;(i >= 0 && lista->Compromisso[i].horario> horario); i--){
+            lista->Compromisso[i+1] = lista->Compromisso[i];
+        }
+        strcpy(lista->Compromisso[i + 1].compromisso,compromisso);
+        lista->Compromisso[i +1].horario = horario;
+        lista->quantidade++;
+        return 1;
+    }
+    return 0;
+}
+
+void exibirListaCompromisso(ListaCompromisso *lista){
+    printf("Lista de todos os compromissos:\n");
+    for(int i = 0; i < lista->quantidade;i++){
+        printf("Nome: %sHorario %d\n--------------\n",lista->Compromisso[i].compromisso,lista->Compromisso[i].horario);
+    }
+}
+
+int excluirCompromisso(ListaCompromisso *lista,char nome[]){
+    for (int i = 0; i < lista->quantidade; i++){
+        if(strcmp(lista->Compromisso[i].compromisso,nome) == 0){
+            for(int j = i; j < lista->quantidade -1; j++){
+                lista->Compromisso[j] = lista->Compromisso[j+1];
+            }
+            lista->quantidade--;
+            return 1;
+        }
+    }
+    return 0;
+}
+
+
+
 
 int main(){
-    
+    int opcao;
+    char nome[30];
+    int horario;
+
+    ListaCompromisso lista;
+
+    iniciarLista(&lista);
+    do{
+        printf("------------ LISTA Compromisso ------------");
+        printf("\n1 - Incluir Compromisso\n2 - Listar compromisso\n3 - Excluir compromisso\n4 - Encerrar programa");
+        printf("\nDigite a opcao: ");
+        scanf("%d",&opcao);
+        getchar();
+        switch (opcao){
+        case 1:
+            printf("\n1 - Incluir compromisso na lista\n");
+            printf("Digite o compromisso: ");
+            fgets(nome,sizeof(nome),stdin);
+            printf("Digite a hora do do compromisso: ");
+            scanf("%d",&horario);
+            incluirCompromisso(&lista,nome,horario);            
+        break;
+        case 2:
+            printf("\n2 - Listar todos Compromissos\n");
+            exibirListaCompromisso(&lista);
+        break;
+        case 3:
+            printf("Digita o compromisso ja realizado para excluir da lista:");
+            fgets(nome,sizeof(nome),stdin);
+            excluirCompromisso(&lista,nome);
+
+            
+            break;
+        case 4:
+            printf("SAIU...");
+            break;
+        default:
+            printf("\nEscolha uma das opcoes validas");
+        }
+
+    }while(opcao != 4);
+
+
 
     return 0;
 }
--- exercico_04/main.c
+++ exercico_04/main.c
@@ -5,6 +5,7 @@
 
 typedef struct{
     char nome[50];
+    int codigo;
     char status;
 }Livro;
 
@@ -17,9 +18,10 @@ void inicializarLista(ListaLivros *lista){
     lista->n = 0;
 }
 
-int incluirLivro(ListaLivros *lista,char nome[],char status){
+int incluirLivro(ListaLivros *lista,char nome[],int codigo,char status){
     if(lista->n < MAX_Livros){
         strcpy(lista->livro[lista->n].nome,nome);
+        lista->livro[lista->n].codigo = codigo;
         lista->livro[lista->n].status = status;
         lista->n++;
         return 1;
@@ -27,9 +29,71 @@ int incluirLivro(ListaLivros *lista,char nome[],char status){
     return 0;
 }
 
+void exibirLivros(ListaLivros *lista){
+    printf("Listar todos os Livros:\n");
+    for(int i = 0; i < lista->n;i++){
+        printf("Nome %sCodigo %d\nStatus %c\n---------------\n",lista->livro[i].nome,lista->livro[i].codigo,lista->livro[i].status);
+    }
+}
+
+int excluirLivros(ListaLivros *lista,char nome[]){
+    for(int i = 0; i < lista->n; i++){
+        if(strcmp(lista->livro[i].nome,nome) == 0 && lista->livro[i].status == 'D'){
+            for(int j = i; j < lista->n -1; j++){
+                lista->livro[j] = lista->livro[j+1];
+            }
+            lista->n--;
+            return 1;
+        }
+    }
+    return 0;
+}
 
 
 int main(){
+    int opcao;
+    char nome[50],status;
+    int codigo;
+
+    ListaLivros lista;
+
+    inicializarLista(&lista);
+
+    do{
+        printf("------------ LISTA Livros ------------");
+        printf("\n1 - Incluir Livro\n2 - Listar Livros\n3 - Excluir Livro\n4 - Encerrar programa");
+        printf("\nDigite a opcao: ");
+        scanf("%d",&opcao);
+        getchar();
+        switch (opcao){
+        case 1:
+            printf("\n1 - Incluir novo Livro\n");
+            printf("Digite o nome do Livro: ");
+            fgets(nome,sizeof(nome),stdin);
+            printf("Digite o codigo do livro: ");
+            scanf("%d",&codigo);
+            getchar();
+            printf("Digite o estado do Livro (D - Danificado, N - Normal): ");
+            scanf("%c",&status);
+            incluirLivro(&lista,nome,codigo,status);            
+        break;
+        case 2:
+            printf("\n2 - Listar todos Livros\n");
+            exibirLivros(&lista);
+        break;
+        case 3:
+            printf("Digita o Livro danificado para excluir da lista:");
+            fgets(nome,sizeof(nome),stdin);
+            excluirLivros(&lista,nome);
+            break;
+        case 4:
+            printf("SAIU...");
+            break;
+        default:
+            printf("\nEscolha uma das opcoes validas");
+        }
+
+    }while(opcao != 4);
     
 
     return 0;
