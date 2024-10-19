92bde490d41e211941e57a9fdde2b38e749beac0
Leonardo-ADS24 <leonardo.santiago@alunos.unis.edu.br>
2024-10-18 23:54:57 -0300

    exe 05,06

--- exercico_05/main.c
+++ exercico_05/main.c
@@ -1,7 +1,72 @@
 #include <stdio.h>
+#include <string.h>
+
+#define MAX 70
+#define NOME_MAX 20
+
+
+
+
+void inserirLista(char lista[][NOME_MAX],int *tamanho, char nome[]){
+    if(*tamanho < MAX){
+        strcpy(lista[*tamanho],nome);
+        (*tamanho)++;
+    }
+
+}
+
+void exibirPacientes(char lista[][NOME_MAX],int tamanho){
+    printf("\nFila dos Pacientes aguardando o atendimento: \n");
+    for(int i = 0; i < tamanho; i++){
+        printf("%s",lista[i]);
+    }
+}
+
+int excluirPaciente(char lista[][NOME_MAX],int *tamanho){
+    if(*tamanho > 0){
+        printf("Paciente %s foi atendido(a) e sera removido da lista",lista[0]);
+        for (int i = 1; i < *tamanho; i++){
+            strcpy(lista[i - 1],lista[i]);
+        }
+        (*tamanho)--;
+    }
+}
+
 
 int main(){
     
+    int opcao,tamanho = 0;
+    char lista[MAX][NOME_MAX];
+    char nome[NOME_MAX];
+
+
+    do{
+        printf("\nDigite sua escolha: \n1 - Adicionar um paciente\n2 - Remover um paciente\n3 - Exibir a lista de pacientes aguardando atendimento\n0 - Sair do programa");
+        printf("\nSua escolha: ");
+        scanf("%d",&opcao);
+        getchar();
+        switch (opcao){
+            case 1:
+                printf("Digita o nome do paciente: ");
+                fgets(nome,sizeof(nome),stdin);
+                inserirLista(lista,&tamanho,nome);
+                break;
+            case 2:
+                printf("Removendo Paciente atendido...\n");
+                excluirPaciente(lista,&tamanho);
+                break;
+            case 3:
+                exibirPacientes(lista,tamanho);
+                break;
+            case 0:
+                printf("SAIU ...\n");
+                break;
+            
+            default:
+                printf("Opcao Invalida");
+                break;
+        }
+    }while(opcao != 0);
 
     return 0;
 }
--- exercico_06/main.c
+++ exercico_06/main.c
@@ -1,6 +1,102 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define NOME_MAX 50
+
+
+typedef struct Produto{
+    char nome[NOME_MAX];
+    float preco;
+    struct Produto* proximo;
+}Produto;
+
+void inserirProduto(Produto** lista,char nome[],float preco){
+    Produto* novoProduto = (Produto*)malloc(sizeof(Produto));
+    strcpy(novoProduto->nome,nome);
+    novoProduto->preco = preco;
+    novoProduto->proximo = NULL;
+
+    if(*lista == NULL || (*lista)->preco >= preco){
+        novoProduto->proximo = *lista;
+        *lista = novoProduto;
+    }else{ 
+        Produto* atual = *lista;
+        while(atual->proximo != NULL && atual->proximo->preco < preco){
+            atual = atual->proximo;
+        }
+        novoProduto->proximo = atual->proximo;
+        atual->proximo = novoProduto;
+        
+    };
+
+}
+
+
+void removerProduto(Produto ** lista,char nome[],float preco){
+    Produto* atual = *lista;
+    Produto* anterior = NULL;
+
+    while(atual != NULL && strcmp(atual->nome,nome) != 0){
+        anterior = atual;
+        atual = atual->proximo;
+    }
+
+    if(anterior == NULL){
+        *lista = atual->proximo;
+    }else{
+        anterior->proximo = atual->proximo;
+    }
+
+    printf("O produto %s com o preco %.2f foi removido\n",nome,preco);
+}
+
+void exibirProdutos(Produto* lista){
+    printf("Os produtos disponiveis sao: \n");
+    Produto* atual = lista;
+    while(atual != NULL){
+        printf("Produto: %sPreco: %.2f\n---------------\n",atual->nome,atual->preco);
+        atual = atual->proximo;
+    }
+}
+
 
 int main(){
+    Produto* lista = NULL;
+    int opcao;
+    char nome[NOME_MAX];
+    float preco;
+
+    do{
+        printf("\nDigite sua escolha: \n1 - Adicionar um produto\n2 - Remover um produto\n3 - Exibir produtos disponiveis\n0 - Sair do programa");
+        printf("\nSua escolha: ");
+        scanf("%d",&opcao);
+        getchar();
+        switch (opcao){
+        case 1:
+            printf("Digita o nome do produto: ");
+            fgets(nome,sizeof(nome),stdin);
+            printf("Digite o preco do produto: ");
+            scanf("%f", &preco);
+            inserirProduto(&lista,nome,preco);
+            break;
+        case 2:
+            printf("Digite o produto que esta indisponivel para remove-lo da lista: ");
+            fgets(nome,sizeof(nome),stdin);
+            removerProduto(&lista,nome,preco);
+            break;
+        case 3:
+            exibirProdutos(lista);
+            break;
+        case 0:
+            printf("SAIU ...\n");
+            break;
+        default:
+            printf("Opcao Invalida");
+            break;
+        }
+
+    }while(opcao != 0);
     
 
     return 0;
